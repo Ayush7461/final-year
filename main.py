b310ea0c792c61c525c189170d81f0ef4b87f22e
Ayush Kumar <68071981+Ayush7461@users.noreply.github.com>
2022-04-03 12:41:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,11 +11,14 @@ Weight = st.number_input("Enter your value of Weight: ",value=50)
 # Height in meter square.
 Height = st.number_input("Enter your value of Height: ",value=150)
 
+# MUAC in Centi Meter
+MUAC=st.number_input("Enter your value of Height: ",value=0)
+
 
 # BMI is the Body Mass Index
 BMI= float(Weight/(Height*Height))
 
-if(BMI < 12):
+if(BMI < 12 or MUAC <12.5):
     st.title('MalNutrition')
 else:
     st.title('Not MalNutrition')
